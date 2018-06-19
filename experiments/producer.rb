026f000cc3b2b3c7f21d3baf9c7e4a03deae3c3a
Rick Carlino <rick.carlino@gmail.com>
2018-06-19 13:22:12 -0500

    ...

--- experiments/producer.rb
+++ experiments/producer.rb
@@ -10,16 +10,18 @@ conn.start
 ch = conn.create_channel
 
 # declare a queue
-binding.pry
-10.times do
-  q  = ch.queue("test1-#{SecureRandom.uuid[0..7]}")
-  5.times do
-    # publish a message to the default exchange which then gets routed to this queue
-    opts = { routing_key: "bot/#{rand(0..999)}/logs" }
-    q.publish("Hello, everybody!", opts)
-  end
 
-  if false # fetch a message from the queue
+10.times do
+  q  = ch.queue("test1-*", auto_delete: true)
+  # 5.times do
+  #   # publish a message to the default exchange which then gets routed to this queue
+  #   opts = {
+  #     routing_key: "bot/#{rand(0..999)}/logs",
+  #     persistent: true
+  #   }
+  #   q.publish("Hello, everybody!", opts)
+  # end
+  20.times do
     delivery_info, metadata, payload = q.pop
     puts "This is the message: #{payload}"
   end
--- experiments/queue_mangler.js
+++ experiments/queue_mangler.js
@@ -1,17 +1,23 @@
-// import { connect } from "mqtt";
-// const URL = "ws://192.168.1.207:3002/ws";
-
-// connect(URL, {});
-const mqtt = require('mqtt');
-var client = mqtt.connect('mqtt://test.mosquitto.org')
+var client = mqtt.connect("ws://localhost:3002/ws");
 
 client.on('connect', function () {
+  console.log("connect");
   client.subscribe('presence')
-  client.publish('presence', 'Hello mqtt')
+  // client.publish('presence', 'this is it!', { qos: 1, durable: true });
 })
+
+client.on("error", function (a, b, c) {
+  console.log("error");
+  d = a;
+  e = b;
+  f = c;
+  debugger;
+});
 
 client.on('message', function (topic, message) {
   // message is Buffer
+  console.log("message");
+
   console.log(message.toString())
   client.end()
 })
